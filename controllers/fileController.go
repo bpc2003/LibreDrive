06f11dbd40884c35388812c51c77ab523dd37569
bpc2003 <wpesfriendnva@gmail.com>
2024-08-02 15:39:37 -0400

    Added File Encryption

--- controllers/fileController.go
+++ controllers/fileController.go
@@ -1,13 +1,18 @@
 package controllers
 
 import (
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/kevinburke/nacl"
+	"github.com/kevinburke/nacl/secretbox"
 	"libredrive/types"
 )
 
@@ -32,13 +37,19 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 	id := int(r.Context().Value("id").(float64))
 	r.ParseMultipartForm(10 << 20)
 
-	file, handler, err := r.FormFile("file")
+	file, handler, err := r.FormFile("upload")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		enc.Encode(types.ErrStruct{Success: false, Msg: err.Error()})
 		return
 	}
 	defer file.Close()
+
+	user, _ := types.Queries.GetUserById(types.CTX, int64(id))
+	key, err := nacl.Load(fmt.Sprintf("%x", sha256.Sum256([]byte(user.Password))))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	buf, err := io.ReadAll(file)
 	if err != nil {
@@ -47,7 +58,8 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err = os.WriteFile("users/"+strconv.Itoa(id)+"/"+handler.Filename, buf, 0750); err != nil {
+	encrypted := secretbox.EasySeal(buf, key)
+	if err = os.WriteFile("users/"+strconv.Itoa(id)+"/"+handler.Filename+".enc", encrypted, 0750); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		enc.Encode(types.ErrStruct{Success: false, Msg: err.Error()})
 	} else {
@@ -60,7 +72,36 @@ func GetFile(w http.ResponseWriter, r *http.Request) {
 	fileName := chi.URLParam(r, "fileName")
 	id := int(r.Context().Value("id").(float64))
 
-	http.ServeFile(w, r, "users/"+strconv.Itoa(id)+"/"+fileName)
+	user, _ := types.Queries.GetUserById(types.CTX, int64(id))
+	key, err := nacl.Load(fmt.Sprintf("%x", sha256.Sum256([]byte(user.Password))))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fp, err := os.Open("users/" + strconv.Itoa(id) + "/" + fileName + ".enc")
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("File doesn't exist"))
+		return
+	}
+	defer fp.Close()
+
+	buf, err := io.ReadAll(fp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Error"))
+		return
+	}
+
+	if buf, err = secretbox.EasyOpen(buf, key); err != nil {
+		log.Fatal(err)
+	} else {
+		fp, _ = os.Create("users/" + strconv.Itoa(id) + "/" + fileName)
+		defer fp.Close()
+		io.WriteString(fp, string(buf))
+		http.ServeFile(w, r, "users/"+strconv.Itoa(id)+"/"+fileName)
+		os.Remove("users/" + strconv.Itoa(id) + "/" + fileName)
+	}
 }
 
 func DeleteFile(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -12,3 +12,8 @@ require (
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.25.0
 )
+
+require (
+	github.com/kevinburke/nacl v0.0.0-20210405173606-cd9060f5f776
+	golang.org/x/sys v0.22.0 // indirect
+)
