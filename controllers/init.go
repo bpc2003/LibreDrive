5c717f2d05175b5d6e8f813a494f01e0672dbac7
bpc2003 <wpesfriendnva@gmail.com>
2024-08-26 14:25:55 -0400

    Add init.go and schema.sql

---
+++ controllers/init.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"context"
+	"database/sql"
+	_ "embed"
+	"log"
+	"os"
+	"path"
+
+	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
+	"libredrive/crypto"
+	"libredrive/models"
+)
+
+var db *sql.DB
+var q *models.Queries
+var ctx = context.Background()
+
+//go:embed schema.sql
+var ddl string
+
+func init() {
+	var err error
+	db, err = sql.Open("sqlite3", "users.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := db.ExecContext(ctx, ddl); err != nil {
+		log.Fatal(err)
+	}
+	q = models.New(db)
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	if u, _ := q.GetUsers(ctx); len(u) == 0 {
+		password, salt := crypto.GeneratePassword(os.Getenv("ADMIN_PASSWORD"), 144)
+		_, err = q.CreateUser(ctx, models.CreateUserParams{Username: "admin", Password: string(password), Salt: salt, Isadmin: true})
+		if err != nil || os.MkdirAll(path.Join("users", "1"), 0750) != nil {
+			log.Fatal(err)
+		}
+	}
+}
